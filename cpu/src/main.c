ac82a6d3c2857c19e8eaade5971900f44f229bd4
NazarenoLopez <nenolz@live.com>
2024-04-03 22:36:32 +0000

    agrego configs

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,8 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <utils/hello.h>
-
+#include <main.h>
 int main(int argc, char* argv[]) {
-    decir_hola("CPU");
-    return 0;
+    
+    cpu_logger = log_create("cpu.log", "CPU", 1, LOG_LEVEL_INFO);
+    log_info(cpu_logger, "Logger CPU Iniciado");
+
+    cpu_config = config_create("cpu.config");
+    if (cpu_config == NULL) {
+        log_error(cpu_logger, "No se pudo abrir el archivo de configuracion");
+        return EXIT_FAILURE;
+    }
+    IP_MEMORIA = config_get_string_value(cpu_config, "IP_MEMORIA");
+    PUERTO_MEMORIA = config_get_string_value(cpu_config, "PUERTO_MEMORIA");
+    PUERTO_ESCUCHA_DISPATCH = config_get_string_value(cpu_config, "PUERTO_ESCUCHA_DISPATCH");
+    PUERTO_ESCUCHA_INTERRUPT = config_get_string_value(cpu_config, "PUERTO_ESCUCHA_INTERRUPT");
+    CANTIDAD_ENTRADAS_TLB = config_get_string_value(cpu_config, "CANTIDAD_ENTRADAS_TLB");
+    ALGORITMO_TLB = config_get_string_value(cpu_config, "ALGORITMO_TLB");
+
+    log_info(cpu_logger, "IP_MEMORIA: %s", IP_MEMORIA);
+    log_info(cpu_logger, "PUERTO_MEMORIA: %s", PUERTO_MEMORIA);
+    log_info(cpu_logger, "PUERTO_ESCUCHA_DISPATCH: %s", PUERTO_ESCUCHA_DISPATCH);
+    log_info(cpu_logger, "PUERTO_ESCUCHA_INTERRUPT: %s", PUERTO_ESCUCHA_INTERRUPT);
+    log_info(cpu_logger, "CANTIDAD_ENTRADAS_TLB: %s", CANTIDAD_ENTRADAS_TLB);
+    log_info(cpu_logger, "ALGORITMO_TLB: %s", ALGORITMO_TLB);
 }
---
+++ cpu/src/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <utils/hello.h>
+#include <commons/log.h>
+#include <commons/config.h>
+
+t_log* cpu_logger;
+t_config* cpu_config;
+char* IP_MEMORIA;
+char* PUERTO_MEMORIA;
+char* PUERTO_ESCUCHA_DISPATCH;
+char* PUERTO_ESCUCHA_INTERRUPT;
+char* CANTIDAD_ENTRADAS_TLB;
+char* ALGORITMO_TLB;
+
+
+#endif
