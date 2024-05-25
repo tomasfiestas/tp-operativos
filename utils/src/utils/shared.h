d0986b6491b1a66e28df0cee2cdb8adb1a08729e
matiasmgh <matiasmgh@gmail.com>
2024-05-25 21:19:04 +0000

    WORK IN PROGRESS de Tomi

--- utils/src/utils/shared.h
+++ utils/src/utils/shared.h
@@ -35,7 +35,18 @@ typedef enum
 
 	//KERNEL
 	//Kernel le avisa a memoria que tiene que crear un proceso
-	CREAR_PROCESO_KM
+	CREAR_PROCESO_KM,
+
+	//Entrada Saldia
+	IO_GEN_SLEEP,
+	IO_STDIN_READ,
+	IO_STDOUT_WRITE,
+	IO_FS_CREATE,
+	IO_FS_DELETE,
+	IO_FS_TRUNCATE,
+	IO_FS_WRITE,
+	IO_FS_READ
+
 }op_code;
 
 // CLIENTE
