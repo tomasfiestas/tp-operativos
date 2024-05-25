12f5203030062b7b1409d33c9346116bb932746d
candelaBe <cberge@frba.utn.edu.ar>
2024-05-24 23:30:06 -0300

    Actualizo funciones en shared y cpu

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -54,6 +54,12 @@ int main(int argc, char* argv[]) {
     //Atender los mensajes de Memoria  
     return EXIT_SUCCESS;
 }
+
+//----------------------
+//       Kernel
+//----------------------
+
+
 void atender_kernel_dispatch(void* socket_cliente_ptr) {
     int cliente_kd = *(int*)socket_cliente_ptr;
     free(socket_cliente_ptr);
@@ -103,4 +109,140 @@ void atender_kernel_interrupt(void* socket_cliente_ptr) {
 			control_key = 0;
 			break;
 	}   } 
+}
+
+
+void esperar_contextos(int *socket_cliente_kernel){
+    while(1){
+
+        t_contexto_ejecucion *ctx;
+        codigo_operacion cod_op = recibir_operacion(*socket_cliente_kernel);
+
+        switch (cod_op)
+        {
+        case CONTEXTO_EJECUCION:
+            
+            ctx = recibir_contexto(*socket_cliente_kernel);
+            empezar_ciclo_instruccion(ctx);
+
+            break;
+        
+        default:
+            log_error(cpu, "No se pudo reconocer la operacion");
+            break;
+        }
+    }
+}
+
+
+void devolver_contexto(t_contexto_ejecucion *ctx,op_code cod_operacion)
+{
+    t_paquete *paquete = crear_paquete(cod_operacion);
+
+    serializar_contexto(paquete,ctx);
+
+    enviar_paquete(paquete, cliente_kernel_dispatch);
+}
+
+
+//To do: completar los demas contextos
+
+
+
+//----------------------
+//       Memoria
+//----------------------
+
+//Separar en archivos
+
+int conexion_memoria;
+
+void atender_memoria(char *ip_memoria, char *puerto_memoria){
+    
+    conexion_memoria = crear_conexion_cliente(ip_memoria, puerto_memoria);
+
+    realizar_handshake(2, conexion_memoria);
+
+    log_info(cpu, "Se conecto con memoria");
+
+    if(recibir_operacion(conexion_memoria) == OK){
+        log_info(cpu, "Memoria envio un OK");
+    }else{
+        log_error(cpu, "No se recibio el tamanio de la pagina de manera correcta");
+    }
+
+    //creo buffer y recibo operacion de memoria
+
+    t_buffer *buffer = crear_buffer();
+    buffer->stream = recibir_buffer(&(buffer->size), conexion_memoria);
+
+    log_info(cpu, "Recibio la operacion de memoria");
+
+    memcpy(&tam_pag, buffer->stream, sizeof(int));
+
+    destruir_buffer(buffer);
+
+}
+
+
+char* leer_de_memoria(int direc_fisica, int pid)
+{
+    t_buffer *buffer = crear_buffer();
+    t_paquete *paquete = crear_paquete(LEER_VALOR, buffer);
+
+    agregar_a_paquete(paquete, &direc_fisica, sizeof(int));
+
+    agregar_a_paquete(paquete, &pid, sizeof(int));
+
+    enviar_paquete(paquete, conexion_memoria);
+
+    op_code codigo_operacion = recibir_operacion(conexion_memoria);
+
+    if(codigo_operacion != WRITE)
+        log_error(cpu, "No se pudo realizar MOV_IN");
+
+    buffer->stream = recibir_buffer(conexion_memoria);
+
+    uint32_t valor_leido;
+    //copio el valor leido del buffer a valor_leido
+    memcpy(&valor_leido, buffer->stream, sizeof(uint32_t));
+
+    char* valor_cadena = (char*)malloc(12 * sizeof(char));
+
+    //uso %u para pasar el uint32_t a char
+    snprintf(valor_cadena, 12, "%u", valor_leido);
+
+    destruir_buffer(buffer);
+    return valor_cadena;    
+}
+
+
+t_instruccion *pedir_instruccion_a_memoria(int pid, uiunt32_t program_counter)
+{
+    t_buffer *buffer = crear_buffer();
+
+    t_paquete *paquete = crear_paquete(SOLICITUD_INST);
+
+    agregar_a_paquete(paquete,&pid, sizeof(int));
+    agregar_a_paquete(paquete, &program_counter, sizeof(uint32_t));
+
+    enviar_paquete(paquete, conexion_memoria);
+
+    //duda:
+    //op_code codigo_operacion = recibir_operacion(conexion_memoria);
+
+    if(codigo_operacion == OK)
+        codigo_operacion = recibir_operacion(conexion_memoria);
+        //deserializar la inst
+    if(codigo_operacion != INSTRUCCION)
+        log_error(cpu, "No se recibio la instruccion de memoria");
+    
+
+    buffer->stream = recibir_buffer(&(buffer->size), conexion_memoria);
+
+    t_instruccion *instruccion = deserializar_instruccion(buffer); //cambiar funcion de deserializar
+
+    destruir_buffer(buffer);
+    return instruccion;
+
 }
